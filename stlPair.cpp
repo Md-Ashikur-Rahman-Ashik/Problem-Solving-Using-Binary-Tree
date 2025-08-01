bbe5ce95e5b91e537230b388297042d0c23be6ff
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-08-01 09:34:49 +0600

    Explored STL pair

--- stlPair.cpp
+++ stlPair.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 int main()
 {
-    pair<int, int> integerPair;
-    integerPair = make_pair(1, 2);
+    // pair<string, int> integerPair;
+    // // integerPair = make_pair(1, 2);
+    // integerPair = {"Hello", 3};
+    // cout << integerPair.first << endl;
+    // cout << integerPair.second << endl;
+
+    int vectorSize;
+    cin >> vectorSize;
+    vector<pair<int, int>> pairVector(vectorSize);
+    for (int i = 0; i < vectorSize; i++)
+    {
+        cin >> pairVector[i].first >> pairVector[i].second;
+    }
+
+    for (int i = 0; i < vectorSize; i++)
+    {
+        cout << pairVector[i].first << " " << pairVector[i].second << endl;
+    }
 
     return 0;
 }
